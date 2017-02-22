5a794a2660d1f86ccf54def4e71d3d1eaa301494
Mehdi Lahlou <mehdi.lahlou@free.fr>
2017-02-22 12:22:58 +0100

    add: add support for gallery array in frontmatter, on pages with Portfolio layout

--- src/layouts/Portfolio/index.js
+++ src/layouts/Portfolio/index.js
@@ -1,16 +1,30 @@
-import React from "react"
+import React, { PropTypes } from "react"
 
 import Gallery from "../../components/Gallery"
 import Page from "../Page"
 
 const Portfolio = (props) => {
+  const galleryElements = props.head.gallery ? props.head.gallery.map((element, index) => {
+    return(
+      <a key={ index } href={ element.url } target="_blank">
+        <img src={ element.image } />
+      </a>
+    )
+  } ) : []
+
   return (
     <Page { ...props }>
       <Gallery>
-        <img src="http://www.thegreenage.co.uk/wp-content/uploads/2013/02/how-to-insulate-a-wall-780x350.jpg" />
+        { galleryElements }
       </Gallery>
     </Page>
   )
+}
+
+Portfolio.propTypes = {
+  head: React.PropTypes.shape({
+    gallery: PropTypes.array
+  })
 }
 
 export default Portfolio
