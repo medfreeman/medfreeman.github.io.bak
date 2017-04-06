e3851b7c56769936890cce669a34b2dcda5e159d
Mehdi Lahlou <mehdi.lahlou@free.fr>
2017-04-06 10:57:34 +0200

    add: add logo to the left of the header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,6 +2,7 @@ import React, { PropTypes } from "react"
 import { Link } from "phenomic"
 import Svg from "react-svg-inline"
 
+import logoSvg from "../icons/medfreeman_3d_dark.svg"
 import twitterSvg from "../icons/iconmonstr-twitter-1.svg"
 import gitHubSvg from "../icons/iconmonstr-github-1.svg"
 
@@ -11,6 +12,12 @@ const Header = (props, { metadata: { pkg } }) => (
   <header className={ styles.header }>
     <nav className={ styles.nav }>
       <div className={ styles.navPart1 }>
+        <Svg
+          className={ styles.logoSvg }
+          svg={ logoSvg }
+          width="50px"
+          height="50px"
+        />
         <Link
           className={ styles.link }
           to={ "/" }
