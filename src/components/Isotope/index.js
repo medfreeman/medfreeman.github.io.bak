37b6688c724f41e8c1e702e75b3d9624e27cf999
Mehdi Lahlou <mehdi.lahlou@free.fr>
2017-04-10 13:12:06 +0200

    add: add imagesloaded support
    Refactor dependency loading
    Refactor elements arranging
    Fix props handling
    Add defaultProps
    Remove unused FilterFns
    Fix indentation

--- src/components/Isotope/index.js
+++ src/components/Isotope/index.js
@@ -3,78 +3,102 @@
 import React, { PropTypes } from "react"
 import _ from "lodash"
 
+const isBrowser = (typeof window !== 'undefined')
+const IsotopeLayout = isBrowser ? require("isotope-layout") : null;
+const imagesloaded = isBrowser ? require("imagesloaded") : null;
+
 class Isotope extends React.PureComponent {
-    constructor(props, context) {
-      super(props, context);
+  constructor(props, context) {
+    super(props, context)
+  }
 
-      this.filterFns = props.filterFns ? props.filterFns : {};
-
-      this.isoOptions = props.isoOptions ? props.isoOptions : {};
+  componentDidMount() {
+    if ( ! isBrowser ) {
+      return
     }
-    componentDidMount() {
-      if ( typeof window === 'undefined' ) {
-        return
-      }
 
-      const Isotope = require("isotope-layout")
+    if (
+      typeof this.props.isoOptions.layoutMode !== 'undefined' &&
+      this.props.isoOptions.layoutMode === 'packery'
+    ) {
+      require("isotope-packery")
 
-      if (
-        typeof this.props.isoOptions.layoutMode !== 'undefined' &&
-        this.props.isoOptions.layoutMode === 'packery'
-      ) {
-        require("isotope-packery")
-
-      }
-
-      if (this.iso == null) {
-        this.iso = new Isotope(this.isotopeContainer, this.isoOptions);
-      }
-
-      // Only arrange if there are elements to arrange
-      if (_.get(this, 'props.children.length', 0) > 0) {
-        this.iso.arrange();
-      }
     }
-    componentDidUpdate(prevProps) {
-      // The list of keys seen in the previous render
-      const currentKeys = _.map(prevProps.children, (n) => n.key);
 
-      // The latest list of keys that have been rendered
-      const newKeys = _.map(this.props.children, (n) => n.key);
-
-      // Find which keys are new between the current set of keys and any new children passed to this component
-      const addKeys = _.difference(newKeys, currentKeys);
-
-      // Find which keys have been removed between the current set of keys and any new children passed to this component
-      const removeKeys = _.difference(currentKeys, newKeys);
-
-      if (removeKeys.length > 0) {
-        _.each(removeKeys, removeKey => this.iso.remove(document.getElementById(removeKey)));
-        this.iso.arrange();
-      }
-      if (addKeys.length > 0) {
-        this.iso.addItems(_.map(addKeys, (addKey) => document.getElementById(addKey)));
-        this.iso.arrange();
-      }
+    if (this.iso == null) {
+      this.iso = new IsotopeLayout(this.isotopeContainer, this.props.isoOptions)
     }
-    componentWillUnmount() {
-      if (this.iso != null) {
-        this.iso.destroy();
-      }
+
+    // Only arrange if there are elements to arrange
+    if (_.get(this, 'props.children.length', 0) > 0) {
+      this.scheduleArrange()
     }
-    render() {
-      return (
-        <div className="isotope" ref={ node => this.isotopeContainer = node }>
-          { this.props.children }
-        </div>
-      )
+  }
+
+  componentDidUpdate(prevProps) {
+    // The list of keys seen in the previous render
+    const currentKeys = _.map(prevProps.children, (n) => n.key)
+
+    // The latest list of keys that have been rendered
+    const newKeys = _.map(this.props.children, (n) => n.key)
+
+    // Find which keys are new between the current set of keys and any new children passed to this component
+    const addKeys = _.difference(newKeys, currentKeys)
+
+    // Find which keys have been removed between the current set of keys and any new children passed to this component
+    const removeKeys = _.difference(currentKeys, newKeys)
+
+    if (removeKeys.length > 0) {
+      _.each(removeKeys, removeKey => this.iso.remove(document.getElementById(removeKey)));
+      this.scheduleArrange()
     }
+    if (addKeys.length > 0) {
+      this.iso.addItems(_.map(addKeys, (addKey) => document.getElementById(addKey)))
+      this.scheduleArrange()
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.iso != null) {
+      this.iso.destroy()
+    }
+  }
+
+  scheduleArrange() {
+    if ( ! this.props.disableImagesLoaded ) {
+      this.imagesLoaded()
+    } else {
+      this.iso.arrange()
+    }
+  }
+
+  imagesLoaded() {
+    imagesloaded(
+      this.isotopeContainer,
+      function() {
+        this.iso.arrange()
+      }.bind(this)
+    );
+  }
+
+  render() {
+    return (
+      <div className="isotope" ref={ node => this.isotopeContainer = node }>
+        { this.props.children }
+      </div>
+    )
+  }
+}
+
+Isotope.defaultProps = {
+  isoOptions: {},
+  disableImagesLoaded: false,
 }
 
 Isotope.propTypes = {
   children: PropTypes.node,
-  filterFns: PropTypes.object,
   isoOptions: PropTypes.object,
+  disableImagesLoaded: React.PropTypes.bool,
 }
 
 export default Isotope
