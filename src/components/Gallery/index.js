8657ee3d4e408e7443fcc2a05cff5218026fd812
Mehdi Lahlou <mehdi.lahlou@free.fr>
2017-04-10 13:14:33 +0200

    change: move children elements to new constructor function, out of render to properly initialize children before isotope rendering
    Refactor to React component instead of stateless component

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -7,60 +7,67 @@ import Isotope from "../Isotope"
 
 import styles from "./index.css"
 
-const Gallery = (props) => {
-  const sizeArray = [
-    "sm",
-    "large",
-    "high",
-    "double"
-  ];
+class Gallery extends React.Component {
+  constructor(props, context) {
+    super(props, context)
 
-  return (
-    <Isotope
-      isoOptions={
-        {
-          itemSelector: `.${styles["gallery-item-container"]}`,
-          layoutMode: 'packery',
-          packery: {
-          },
-        }
+    this.sizeArray = [
+      "sm",
+      "large",
+      "high",
+      "double"
+    ]
+
+    this.elements = this.props.elements.map((element, index) => {
+      let containerClass = "gallery-item-container--sm"
+      if ( ! /\.(png|jpe?g|svg)$/.test(element.image) ) {
+        const imageKey = path.basename(element.image)
+        const imageSize = this.sizeArray[Math.floor(Math.random()*this.sizeArray.length)]
+        containerClass = `gallery-item-container--${imageSize}`
+        element.image = path.join(element.image, `${imageKey}-${imageSize}.png` )
       }
-    >
-      {
-        props.elements.map((element, index) => {
-          let containerClass = "gallery-item-container--sm"
-          if ( ! /\.(png|jpe?g|svg)$/.test(element.image) ) {
-            const imageKey = path.basename(element.image)
-            const imageSize = sizeArray[Math.floor(Math.random()*sizeArray.length)]
-            containerClass = `gallery-item-container--${imageSize}`
-            console.log(containerClass)
-            element.image = path.join(element.image, `${imageKey}-${imageSize}.png` )
+      return (
+        <div
+          key={ index }
+          className={
+            cx(
+              styles["gallery-item-container"],
+              styles[containerClass],
+            )
           }
-          return (
-            <div
-              key={ index }
-              className={
-                cx(
-                  styles["gallery-item-container"],
-                  styles[containerClass],
-                )
-              }
+        >
+          <div className={ styles["gallery-item"] }>
+            <a
+              href={ element.url }
+              target="_blank"
+              rel="noreferrer noopener"
             >
-              <div className={ styles["gallery-item"] }>
-                <a
-                  href={ element.url }
-                  target="_blank"
-                  rel="noreferrer noopener"
-                >
-                  <img src={ element.image } />
-                </a>
-              </div>
-            </div>
-          )
-        })
-      }
-    </Isotope>
-  )
+              <img src={ element.image } />
+            </a>
+          </div>
+        </div>
+      )
+    })
+  }
+
+  render() {
+    return (
+      <Isotope
+        isoOptions={
+          {
+            itemSelector: `.${styles["gallery-item-container"]}`,
+            layoutMode: 'packery',
+            packery: {
+            },
+          }
+        }
+      >
+        {
+          this.elements
+        }
+      </Isotope>
+    )
+  }
 }
 
 Gallery.propTypes = {
